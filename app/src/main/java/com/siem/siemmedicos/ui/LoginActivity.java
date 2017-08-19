d9f14efec59573b51d92d9b50cd30b57bc525a14
Lucas <frontalinilucas@gmail.com>
2017-08-18 21:57:03 -0300

    Login

--- app/src/main/java/com/siem/siemmedicos/ui/LoginActivity.java
+++ app/src/main/java/com/siem/siemmedicos/ui/LoginActivity.java
@@ -4,12 +4,14 @@ import android.app.Activity;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Toast;
 
+import com.google.firebase.database.Transaction;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.siem.siemmedicos.R;
 import com.siem.siemmedicos.databinding.ActivityLoginBinding;
@@ -18,11 +20,14 @@ import com.siem.siemmedicos.utils.Constants;
 import com.siem.siemmedicos.utils.PreferencesHelper;
 import com.siem.siemmedicos.utils.RetrofitClient;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends Activity implements Callback<LoginResponse> {
 
     private PreferencesHelper mPreferences;
     private ActivityLoginBinding mBinding;
@@ -36,36 +41,86 @@ public class LoginActivity extends Activity {
         mBinding.buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Animation shake = AnimationUtils.loadAnimation(LoginActivity.this, R.anim.vibrate);
-                //mBinding.textinputlayoutUser.startAnimation(shake);
-                Call<LoginResponse> call = RetrofitClient.getServerClient().login(mBinding.edittextUser.getText().toString(), mBinding.edittextPass.getText().toString());
-                call.enqueue(new Callback<LoginResponse>() {
-                    @Override
-                    public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
-                        switch(response.code()){
-                            case Constants.CODE_SERVER_OK:
-                                goToMap();
-                                break;
-                            case Constants.CODE_BAD_REQUEST:
-                                Toast.makeText(LoginActivity.this, "User o pass incorrecto", Toast.LENGTH_LONG).show();
-                                break;
-                            default:
-                                error();
-                                break;
+                if(isLoginFullComplete()){
+                    habilitarLoading();
+                    Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            String user = mBinding.edittextUser.getText().toString();
+                            String pass = mBinding.edittextPass.getText().toString();
+                            Call<LoginResponse> call = RetrofitClient.getServerClient().login(user, pass);
+                            call.enqueue(LoginActivity.this);
                         }
-                    }
-
-                    @Override
-                    public void onFailure(Call<LoginResponse> call, Throwable t) {
-                        error();
-                    }
-                });
+                    }, 100);
+                }
             }
         });
     }
 
-    private void error() {
-        Toast.makeText(this, "ERROR", Toast.LENGTH_LONG).show();
+    @Override
+    public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
+        switch(response.code()){
+            case Constants.CODE_SERVER_OK:
+                goToMap();
+                break;
+            case Constants.CODE_BAD_REQUEST:
+                loginError();
+                break;
+            default:
+                serverError();
+                break;
+        }
+    }
+
+    @Override
+    public void onFailure(Call<LoginResponse> call, Throwable t) {
+        serverError();
+    }
+
+    private boolean isLoginFullComplete() {
+        String user = mBinding.edittextUser.getText().toString();
+        String pass = mBinding.edittextPass.getText().toString();
+        List<View> listErrorView = new ArrayList<>();
+        if(user.isEmpty())
+            listErrorView.add(mBinding.edittextUser);
+        if(pass.isEmpty())
+            listErrorView.add(mBinding.edittextPass);
+
+        vibrateToError(listErrorView);
+        return listErrorView.size() <= 0;
+    }
+
+    private void vibrateToError(List<View> listErrorView) {
+        if(listErrorView.size() > 0){
+            Toast.makeText(LoginActivity.this, getString(R.string.errorEmptyFields), Toast.LENGTH_LONG).show();
+            //Animation shake = AnimationUtils.loadAnimation(LoginActivity.this, R.anim.vibrate);
+            //mBinding.textinputlayoutUser.startAnimation(shake);
+        }
+    }
+
+    private void serverError(){
+        deshabilitarLoading();
+        Toast.makeText(this, getString(R.string.error), Toast.LENGTH_LONG).show();
+    }
+
+    private void loginError() {
+        deshabilitarLoading();
+        Toast.makeText(LoginActivity.this, getString(R.string.errorLogin), Toast.LENGTH_LONG).show();
+    }
+
+    private void habilitarLoading() {
+        mBinding.buttonLogin.setEnabled(false);
+        mBinding.edittextPass.setEnabled(false);
+        mBinding.edittextUser.setEnabled(false);
+        mBinding.progressContent.setVisibility(View.VISIBLE);
+    }
+
+    private void deshabilitarLoading() {
+        mBinding.buttonLogin.setEnabled(true);
+        mBinding.edittextPass.setEnabled(true);
+        mBinding.edittextUser.setEnabled(true);
+        mBinding.progressContent.setVisibility(View.GONE);
     }
 
     private void controlarMedicoLogueado() {
